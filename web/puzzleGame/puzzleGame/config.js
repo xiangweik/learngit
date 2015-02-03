fda9e89dc488b173230361fd438b3836a38964f9
xiangwei <xiangweik@163.com>
2015-02-03 08:46:13 +0800

    puzzleGameWeb
    创建web版本库

---
+++ web/puzzleGame/puzzleGame/config.js
@@ -0,0 +1,17 @@
+﻿function configInfo()
+{
+	var configObj = new Object();
+	configObj.GamePath = "../puzzleWebSource/";
+	configObj.ConfigVersion = 1;
+	configObj.SWFPath = "../puzzleWebSource/abcswf/";
+	configObj.StylePath = "../puzzleWebSource/style/";
+	configObj.GameWidth = 1000;
+	configObj.GameHeight = 600;
+	return configObj;
+}
+function gameOver(score){
+	alert("score: " + score);
+}
+
+
+
